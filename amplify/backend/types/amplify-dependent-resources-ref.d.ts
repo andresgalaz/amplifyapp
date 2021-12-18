f5b6b7c45b663812a42eee54c042f7fa14f3d7f5
andresgalaz <andres.galaz@gmail.com>
2021-12-17 22:35:58 -0300

    fin tutorial

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifyapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "amplifyappfd3cba05": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "imagestorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,6 +3,10 @@
 
 const awsmobile = {
     "aws_project_region": "us-west-2",
+    "aws_appsync_graphqlEndpoint": "https://igybkfjnivdkxac5qxmuofejse.appsync-api.us-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "us-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-edzsja4uongpvhxksmklterzra",
     "aws_cognito_identity_pool_id": "us-west-2:3a358646-b76f-422f-ac62-a07ef9543f0c",
     "aws_cognito_region": "us-west-2",
     "aws_user_pools_id": "us-west-2_lIjSIP5uK",
@@ -25,7 +29,9 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_user_files_s3_bucket": "bckandy15523-staging",
+    "aws_user_files_s3_bucket_region": "us-west-2"
 };
 
 
